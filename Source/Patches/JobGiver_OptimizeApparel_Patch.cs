ac25ecbed4bc89c54aeb953bc83e362bbed82aa5
notfood <notfood@users.noreply.github.com>
2018-12-30 16:56:01 -0500

    ApparelScoreRaw null pawn check

--- Source/Patches/JobGiver_OptimizeApparel_Patch.cs
+++ Source/Patches/JobGiver_OptimizeApparel_Patch.cs
@@ -8,6 +8,11 @@ namespace Outfitted
     static class JobGiver_OptimizeApparel_ApparelScoreRaw_Patch
     {
         static bool Prefix(Pawn pawn, Apparel ap, out float __result, NeededWarmth ___neededWarmth) {
+            if (pawn == null) {
+                __result = float.NaN;
+
+                return true;
+            }
 
             __result = OutfittedMod.ApparelScoreRaw(pawn, ap, ___neededWarmth);
 
